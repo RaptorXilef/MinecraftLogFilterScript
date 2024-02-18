2ca93cec942bc489404225e0a730c670b29226ec
RaptorXilef <31962607+RaptorXilef@users.noreply.github.com>
2024-02-18 23:02:04 +0100

    modify update-info_v0.0.4

--- new_ideas_(public)/include_in_MinecraftLogFilterScript_in_future/update-info_v0.0.4.ps1
+++ new_ideas_(public)/include_in_MinecraftLogFilterScript_in_future/update-info_v0.0.4.ps1
@@ -1,30 +1,39 @@
 # Definition der Funktion Get-LatestVersionFromGitHub
 function Get-LatestVersionFromGitHub($releaseUrlApi) {
-    $response = Invoke-RestMethod -Uri $releaseUrlApi -Method Get
-    $latestVersion = $response.tag_name
-    return $latestVersion
+    try {
+        $response = Invoke-RestMethod -Uri $releaseUrlApi -Method Get
+        $latestVersion = $response.tag_name
+        return $latestVersion
+    }
+    catch {
+        Write-Host "GitHub API von MinecraftLogFilterScript nicht erreichbar." -ForegroundColor Red
+        Write-Host "Es konnte nicht geprüft werden ob ein Update verfügbar ist." -ForegroundColor Red
+        return $null
+    }
 }
 
 # Definition der Funktion CheckForUpdate
 function CheckForUpdate($installierteVersion, $neuesteVersion, $repoOwner, $repoName, $releaseUrl) {
-    if ($neuesteVersion -gt $installierteVersion) {
-        Write-Host "Es ist ein Update verfügbar!"
-        Write-Host "Installierte Version: $installierteVersion, Neueste Version: $neuesteVersion"
-        Write-Host "GitHub Projektseite: https://github.com/$repoOwner/$repoName"
-        
-        Write-Host "Neuestes Release: $releaseUrl"
-        $antwort = Read-Host "Möchten Sie die Seite zum Release öffnen? (J/N)"
-        if ($antwort -eq "J" -or $antwort -eq "j") {
-            Start-Process $releaseUrl
+    if ($neuesteVersion) {
+        if ($neuesteVersion -gt $installierteVersion) {
+            Write-Host "Es ist ein Update verfügbar!"
+            Write-Host "Installierte Version: $installierteVersion, Neueste Version: $neuesteVersion"
+            Write-Host "GitHub Projektseite: https://github.com/$repoOwner/$repoName"
+            
+            Write-Host "Neuestes Release: $releaseUrl"
+            $antwort = Read-Host "Möchten Sie die Seite zum Release öffnen? (J/N)"
+            if ($antwort -eq "J" -or $antwort -eq "j") {
+                Start-Process $releaseUrl
+            }
+            else {
+                Write-Host "Öffnen Sie die Seite $releaseUrl, um das neueste Release anzuzeigen."
+                Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren ..."
+            }
         }
         else {
-            Write-Host "Öffnen Sie die Seite $releaseUrl, um das neueste Release anzuzeigen."
+            Write-Host "Die installierte Version ($installierteVersion) ist auf dem neuesten Stand."
             Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren ..."
         }
-    }
-    else {
-        Write-Host "Die installierte Version ($installierteVersion) ist auf dem neuesten Stand."
-        Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren ..."
     }
 }
 
