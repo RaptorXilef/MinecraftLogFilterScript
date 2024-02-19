0af4b0a9f49d66cd438c69a4397b5046de571423
RaptorXilef <31962607+RaptorXilef@users.noreply.github.com>
2024-02-19 23:25:51 +0100

    File added to continue working on the go

--- my_test_area_to_continue_working_on_the_go/MinecraftLogFilterScript_with_update-notification.ps1
+++ my_test_area_to_continue_working_on_the_go/MinecraftLogFilterScript_with_update-notification.ps1
@@ -4,6 +4,79 @@
 # GitHub: https://github.com/RaptorXilef/MinecraftLogFilterScript
 # REM Lizens: GNU GENERAL PUBLIC LICENSE - Version 3, 29 June 2007
 
+# ! Update-Skript aus den Tests
+# Definition der Funktion Get-LatestVersionFromGitHub
+function Get-LatestVersionFromGitHub($releaseUrlApi) {
+    try {
+        $response = Invoke-RestMethod -Uri $releaseUrlApi -Method Get
+        $latestVersion = $response.tag_name
+        return $latestVersion
+    }
+    catch {
+        Write-Host "GitHub API von MinecraftLogFilterScript nicht erreichbar." -ForegroundColor Red
+        Write-Host "Es konnte nicht geprüft werden ob ein Update verfügbar ist." -ForegroundColor Red
+        return $null
+    }
+}
+
+# Definition der Funktion CheckForUpdate
+function CheckForUpdate($installierteVersion, $neuesteVersion, $repoOwner, $repoName, $releaseUrl) {
+    if ($neuesteVersion) {
+        if ($neuesteVersion -gt $installierteVersion) {
+            Write-Host "Es ist ein Update verfügbar!"
+            Write-Host "Installierte Version: $installierteVersion, Neueste Version: $neuesteVersion"
+            Write-Host "GitHub Projektseite: https://github.com/$repoOwner/$repoName"
+            
+            Write-Host "Neuestes Release: $releaseUrl"
+            $antwort = Read-Host "Möchten Sie die Seite zum Release öffnen? (J/N)"
+            if ($antwort -eq "J" -or $antwort -eq "j") {
+                Start-Process $releaseUrl
+            }
+            else {
+                Write-Host "Öffnen Sie die Seite $releaseUrl, um das neueste Release anzuzeigen."
+                Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren ..."
+            }
+        }
+        else {
+            Write-Host "Die installierte Version ($installierteVersion) ist auf dem neuesten Stand."
+            Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren ..."
+        }
+    }
+}
+
+# Variablen
+$installierteVersion = "v0.0.9"
+$repoOwner = "RaptorXilef"
+$repoName = "MinecraftLogFilterScript"
+$releaseUrlApi = "https://api.github.com/repos/$repoOwner/$repoName/releases/latest"
+$releaseUrl = "https://github.com/$repoOwner/$repoName/releases/latest"
+
+# Aufruf der Funktion Get-LatestVersionFromGitHub
+$neuesteVersion = Get-LatestVersionFromGitHub $releaseUrlApi
+
+# Aufruf der Funktion CheckForUpdate
+CheckForUpdate $installierteVersion $neuesteVersion $repoOwner $repoName $releaseUrl
+
+# Hier kommt dann der restliche Teil des Skripts
+Read-Host "Drücken Sie eine beliebige Taste, um fortzufahren ..."
+
+# ! Ende Update-Skript aus den Tests
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ! Skript von Version 0.0.1
+# ToDo Skript neu aufbauen, nach den neu erlernten konventionen: erst Funktionen definieren, dann Variablen laden, dann Funktionen ausführen. Nach diesem Shema ändern!
+
 # Aktuelle Skriptversion
 $minecraftLogFilterScriptVersion = "0.0.1"
 
